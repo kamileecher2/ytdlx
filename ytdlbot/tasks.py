2ceac24474f1ac66cd0ffacaf2b3209cf59bfdf8
kamileecher2 <91373907+kamileecher2@users.noreply.github.com>
2022-02-20 22:41:47 +0300

    Update tasks.py

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -334,9 +334,10 @@ def gen_cap(chat_id, url, video_path):
             thumb=getattr(video_path, "thumb", None),
         )
     remain = bot_text.remaining_quota_caption(chat_id)
+    remain = bot_text.remaining_quota_caption(chat_id)
     worker = get_dl_source()
-            cap = f"`📕Adi: {filename}`\n\nℹBilgi: {meta['width']}x{meta['height']} {size} {meta['duration']}s" \
-                  f"\n{remain}\n{worker}"
+    cap = f"`{file_name}`\n\n{url}\n\n{meta['width']}x{meta['height']} {file_size}\t" \
+          f"{meta['duration']}s\n{remain}\n{worker}"
     return cap, meta
 
 
