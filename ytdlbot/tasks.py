62058c4a2f78394c6e49c514961c7b2a3383b508
kamileecher2 <91373907+kamileecher2@users.noreply.github.com>
2022-02-20 20:36:02 +0300

    Update tasks.py

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -335,8 +335,7 @@ def gen_cap(chat_id, url, video_path):
         )
     remain = bot_text.remaining_quota_caption(chat_id)
     worker = get_dl_source()
-    cap = f"`{file_name}`\n{file_size}\t" \
-          f"{meta['duration']}s\n{remain}\n{worker}"
+    cap = f"`{file_name}`\n{file_size}\t"
     return cap, meta
 
 
